fb31500ebd847b708999c184f0bb6ed85d959050
Vanessa Kasminoff <sunnset07@gmail.com>
2023-12-07 22:40:01 -0800

    Created a car class that extends the vehicle class and added methods

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,9 +1,11 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+
+// const VehicleModule = require("./vehicleBaseClass")
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+
+// let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
+// console.log(v.make)
 
 
 //After you write the derived Car class, you should test it out.
@@ -13,10 +15,51 @@ console.log(v.make)
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
 
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passengers = 0;
+        this.numbOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
 
+    loadPassenger(num) {
+        if (this.passengers < this.maxPassengers) {
+            if ((num + this.passengers) <= this.maxPassengers) {
+                this.passengers = num;
+                return this.passengers;               
+            } else {
+                console.log(`${this.make} ${this.model} does not have enough space to take all passengers.`);
+            }
+        } else {
+            console.log(`${this.make} ${this.model} is full.`);
+        }
+    }
 
+    start() {
+        if(this.fuel > 0) {
+            return this.started = true;
+        } else {
+            return this.started = false;
+        }
+    }
 
+    checkService() {
+        if(this.mileage > 30000) {
+            this.scheduleService = true;
+            return this.scheduleService;
+        }
+    }
 
+}
+
+let myWhip = new Car("Honda", "Civic", "2019", "White", "56000");
+console.log(myWhip);
+console.log(myWhip.checkService());
+console.log(myWhip.loadPassenger(5));
 
 
 
